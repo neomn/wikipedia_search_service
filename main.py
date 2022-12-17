004d289a9cc892e548290a9ddee4a47f385976c2
neomn110 <neomn110@gmail.com>
2022-12-17 12:04:07 +0330

    feat: run server

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+import uvicorn
 from src.mylib.logic import search_wiki
 
 app = FastAPI()
+
+
 # print(search_wiki())
 
 @app.get('/')
 async def root():
-    return {'wikipedia search service'}
+    return {'wikipedia search service , call /search/value endpoint to search in wikipedia'}
+
+
+@app.get('/search/{value}')
+async def search(value: str):
+    """search in wikipedia for given value"""
+    return search_wiki(value, 1)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
